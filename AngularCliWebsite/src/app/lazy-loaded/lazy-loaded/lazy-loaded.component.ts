a64f2c7a728f2ce52e8b1263d4081c201a060ad9
JamesWebDev <cylusomally@gmail.com>
2017-01-18 21:58:33 -0500

    Show how simple adding js libraries is
    just npm install, and import aka js's using statment

--- AngularCliWebsite/src/app/lazy-loaded/lazy-loaded/lazy-loaded.component.ts
+++ AngularCliWebsite/src/app/lazy-loaded/lazy-loaded/lazy-loaded.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as _ from "lodash";
 @Component({
   selector: 'app-lazy-loaded',
   templateUrl: './lazy-loaded.component.html',
   styleUrls: ['./lazy-loaded.component.scss']
 })
 export class LazyLoadedComponent implements OnInit {
-
+  lodashTextExample:string;
   constructor() { }
 
   ngOnInit() {
+    this.lodashTextExample = _.join(['a', 'b', 'c'], '~');
   }
-
 }
